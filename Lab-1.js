fd15373067bba84e4ea635c4a0721c20f33f0ef8
ZamzamF <zamzam.fahiyee@outlook.com>
2024-12-04 11:12:27 +0100

    Modified html and js code

---
+++ Lab-1.js
@@ -0,0 +1,17 @@
+//------ Uppgift 4 --------
+
+//1. Hämtar upp checboxen
+let checkboxData = document.getElementById("divStyle");
+
+//2. Hämtar upp alla element med classen "textField"
+let textFieldElements = document.getElementsByClassName("textfield"); 
+
+//3. Hämtar upp tabort-knappen
+let removebtn = document.getElementsByTagName("button");
+
+//4. Hämtar upp emptyDiv
+let displayDiv = document.querySelector("#emptyDiv");
+
+
+
+//------ Uppgift 5 --------
