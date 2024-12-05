b211313fcf70c381e7823d446fa7df819f39860a
reza-lashkari <h23rezla@du.se>
2024-12-05 13:01:22 +0100

    day 2

--- Lab-1.js
+++ Lab-1.js
@@ -15,3 +15,44 @@ let displayDiv = document.querySelector("#emptyDiv");
 
 
 //------ Uppgift 5 --------
+
+
+function handleContentField(e) {
+//e.target = elementet där det har hänt någonting 
+    console.log("event genererat av:",e.target);
+    // hämtar namnet från innehålsfältet och spara det i en variabel.
+    let contentField = e.target.name;
+    console.log("namn artibutet som hämtades upp är ", contentField);
+
+    // kontrollerar om namn atributet är content
+    if (contentField == "content") {
+        //hämtar värdet från content fältet
+        let inputvalue = e.target.value;
+        //skriver ut det hämtade värdet till display div
+        displayDiv.innerHTML = inputvalue;
+    }
+}
+
+//en annonym fuktion till checkboxen som körs vid eventet change och målet är när checkboxen är märkerat ska display div agerar 
+
+checkboxData.addEventListener("change", function () {
+    //hämtar upp fältet med id color 
+    let colorField = document.getElementById("color");
+    // hämtar upp den valde färgen och sparar det i en variabel
+    let chosenColor = colorField.value;
+    // ändrar färg på display diven
+    displayDiv.style.backgroundColor= chosenColor;
+});
+
+// Koppla eventlyssnare handel content field i text fältet 
+for (let i=0 ; i < textFieldElements.length ; i++) {
+    textFieldElements[i].addEventListener("input", handleContentField) ;
+}
+
+
+
+// Event Listener for Remove Button
+removebtn.addEventListener("click", function (){
+    displayDiv.innerHTML= "";
+  });
+  
