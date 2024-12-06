abfe3d85a082e36e2d50c01d4fdcb0af6b0e11ec
ZamzamF <zamzam.fahiyee@outlook.com>
2024-12-06 11:49:17 +0100

    Finished code

--- Lab-1.js
+++ Lab-1.js
@@ -1,13 +1,13 @@
 //------ Uppgift 4 --------
 
 //1. Hämtar upp checboxen
-let checkboxData = document.getElementById("divStyle");
+let checkboxData = document.querySelector("#divStyle");
 
 //2. Hämtar upp alla element med classen "textField"
-let textFieldElements = document.getElementsByClassName("textfield"); 
+let textFieldElements = document.querySelectorAll(".textfield")
 
 //3. Hämtar upp tabort-knappen
-let removebtn = document.getElementsByTagName("button");
+let removebtn = document.getElementById("removeButton");
 
 //4. Hämtar upp emptyDiv
 let displayDiv = document.querySelector("#emptyDiv");
@@ -15,13 +15,11 @@ let displayDiv = document.querySelector("#emptyDiv");
 
 
 //------ Uppgift 5 --------
-
-
 function handleContentField(e) {
 //e.target = elementet där det har hänt någonting 
     console.log("event genererat av:",e.target);
     // hämtar namnet från innehålsfältet och spara det i en variabel.
-    let contentField = e.target.name;
+    const contentField = e.target.name; 
     console.log("namn artibutet som hämtades upp är ", contentField);
 
     // kontrollerar om namn atributet är content
@@ -33,26 +31,39 @@ function handleContentField(e) {
     }
 }
 
-//en annonym fuktion till checkboxen som körs vid eventet change och målet är när checkboxen är märkerat ska display div agerar 
-
+//------ Uppgift 6 Del 1 --------
+//Eventlyssnare för checkboxen
 checkboxData.addEventListener("change", function () {
+    let chosenColor = "";
     //hämtar upp fältet med id color 
-    let colorField = document.getElementById("color");
-    // hämtar upp den valde färgen och sparar det i en variabel
-    let chosenColor = colorField.value;
-    // ändrar färg på display diven
+    for (let i = 0; i < textFieldElements.length; i++) {
+        if (textFieldElements[i].id === "color"){
+            // hämtar upp den valde färgen och sparar det i en variabel
+            chosenColor = textFieldElements[i].value; 
+            break;
+        }
+    }
+    // Ändrar färg på display diven
     displayDiv.style.backgroundColor= chosenColor;
 });
 
-// Koppla eventlyssnare handel content field i text fältet 
+
+//------ Uppgift 6 Del 2 --------
+// Kopplar funtionen "handleContentField" till båda textfälten
 for (let i=0 ; i < textFieldElements.length ; i++) {
     textFieldElements[i].addEventListener("input", handleContentField) ;
 }
 
 
+//------ Uppgift 6 Del 3 --------
+// Event Listener för knappen.
+removebtn.addEventListener("click", function (e){
+    
+    //Gör att knappen inte beter sig som submit-knapp.
+    e.preventDefault();
 
-// Event Listener for Remove Button
-removebtn.addEventListener("click", function (){
-    displayDiv.innerHTML= "";
+    //Tar bort div elementet från DOM-Trädet
+    displayDiv.remove();
+
   });
   
